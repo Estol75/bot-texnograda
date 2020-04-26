161f6692569960c1ffbc601a48c291a138c54472
Estol75 <64350850+Estol75@users.noreply.github.com>
2020-04-26 10:35:48 +0200

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,9 +1,59 @@
+import asyncio
 import discord
-import discord.ext import commands
+from discord import utils
+import config
 import os
 
-bot = commands.Bot(command_prefix= "!")
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        client.loop.create_task(status_task())
+    async def on_raw_reaction_add(self, payload):
+        if payload.message_id == config.POST_ID:
+            channel = self.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            member = utils.get(message.guild.members, id=payload.user_id)
+            try:
+                emoji = str(payload.emoji)
+                role = utils.get(message.guild.roles, id=config.ROLES[emoji])
+                if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER):
+                    await member.add_roles(role)
+                    print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
+                else:
+                    await message.remove_reaction(payload.emoji, member)
+                    print('[ERROR] Too many roles for user {0.display_name}'.format(member))
+            except KeyError as e:
+                print('[ERROR] KeyError, no role found for ' + emoji)
+            except Exception as e:
+                print(repr(e))
+    async def on_raw_reaction_remove(self, payload):
+        channel = self.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        member = utils.get(message.guild.members, id=payload.user_id)
+        try:
+            emoji = str(payload.emoji)
+            role = utils.get(message.guild.roles, id=config.ROLES[emoji])
+            await member.remove_roles(role)
+            print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
+        except KeyError as e:
+            print('[ERROR] KeyError, no role found for ' + emoji)
+        except Exception as e:
+            print(repr(e))
+
+async def status_task():
+    while True:
+        await client.change_presence(activity=discord.Game('Дайте боту все права пжпжпж'),status=discord.Status.online)
+        await asyncio.sleep(3)
+        await client.change_presence(activity=discord.Game('и на верх меня пжпжжп'),status=discord.Status.online)
+        await asyncio.sleep(3)
+        await client.change_presence(activity=discord.Game('кста ты далбаеб ой'),status=discord.Status.online)
+        await asyncio.sleep(3)
 
 
+
+
+
+
+client = MyClient()
 token = os.environ.get('BOT_TOKEN')
-bot.run(token)
+client.run(token)
